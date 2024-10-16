086b7a806a0771da9b2ff4780eebf9b3d86aa039
theussoares <matheusfs774@gmail.com>
2024-10-16 14:26:50 -0400

    feat: funções

---
+++ script.js
@@ -0,0 +1,25 @@
+let notas = [9, 10, 8, 2, 1, 9, 0, 6, 7, 3, 8];
+
+const notasAcimaDaMedia = () => {
+
+};
+
+const NotasNaMedia = () => {
+
+};
+
+const notasAbaixoDaMedia = () => {
+
+};
+
+const somaDeTodasAsNotas = () => {
+
+};
+
+const maiorNota = () => {
+
+};
+
+const menorNota = () => {
+
+}
